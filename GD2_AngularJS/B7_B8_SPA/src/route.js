0d5c3fd6ea2e6c1dc438068a519b1aa484b2dee4
HangNT169 <HangNT169@fpt.edu.vn>
2023-02-02 15:54:09 +0700

    B7_SPA1

--- GD2_AngularJS/B7_B8_SPA/src/route.js
+++ GD2_AngularJS/B7_B8_SPA/src/route.js
@@ -1,47 +1,114 @@
 var myApp = angular.module("myModule", ["ngRoute"]);
 // chuyen trang
 myApp.config(function ($routeProvider, $locationProvider) {
-  // xoa tren duong dan
+  // xoa khoang trang trinh duyet
   $locationProvider.hashPrefix("");
-  // chuyen trang : $routeProvider
+  // $routeProvider : chuyen trang
   $routeProvider
     .when("/trang-chu", {
       // lam gi thi lam
       // noi dung cua trang chu
       template: `<section>
-              <h1>Noi dung trang chu</h1>
-               <h1>Noi dung trang chu1111</h1>
-               <h1>Noi dung trang chu1111</h1>
-               <table>
-               <tr>
-                 <th>Company</th>
-                 <th>Contact</th>
-                 <th>Country</th>
-               </tr>
-               <tr>
-                 <td>Alfreds Futterkiste</td>
-                 <td>Maria Anders</td>
-                 <td>Germany</td>
-               </tr>
-               <tr>
-                 <td>Centro comercial Moctezuma</td>
-                 <td>Francisco Chang</td>
-                 <td>Mexico</td>
-               </tr>
-             </table>
-          </section>`,
+            <h1>Noi dung trang chu</h1>
+            <h1>Noi dung trang chu</h1>
+            <h1>Noi dung trang chu</h1>
+            <h1>Noi dung trang chu</h1>
+            <h1>Noi dung trang chu</h1>
+        </section>
+        <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table><table>
+<tr>
+  <th>Company</th>
+  <th>Contact</th>
+  <th>Country</th>
+</tr>
+<tr>
+  <td>Alfreds Futterkiste</td>
+  <td>Maria Anders</td>
+  <td>Germany</td>
+</tr>
+<tr>
+  <td>Centro comercial Moctezuma</td>
+  <td>Francisco Chang</td>
+  <td>Mexico</td>
+</tr>
+</table><table>
+<tr>
+  <th>Company</th>
+  <th>Contact</th>
+  <th>Country</th>
+</tr>
+<tr>
+  <td>Alfreds Futterkiste</td>
+  <td>Maria Anders</td>
+  <td>Germany</td>
+</tr>
+<tr>
+  <td>Centro comercial Moctezuma</td>
+  <td>Francisco Chang</td>
+  <td>Mexico</td>
+</tr>
+</table><table>
+<tr>
+  <th>Company</th>
+  <th>Contact</th>
+  <th>Country</th>
+</tr>
+<tr>
+  <td>Alfreds Futterkiste</td>
+  <td>Maria Anders</td>
+  <td>Germany</td>
+</tr>
+<tr>
+  <td>Centro comercial Moctezuma</td>
+  <td>Francisco Chang</td>
+  <td>Mexico</td>
+</tr>
+</table><table>
+<tr>
+  <th>Company</th>
+  <th>Contact</th>
+  <th>Country</th>
+</tr>
+<tr>
+  <td>Alfreds Futterkiste</td>
+  <td>Maria Anders</td>
+  <td>Germany</td>
+</tr>
+<tr>
+  <td>Centro comercial Moctezuma</td>
+  <td>Francisco Chang</td>
+  <td>Mexico</td>
+</tr>
+</table>
+        `,
     })
-    .when("/chung-toi", {
+    .when("/contact", {
       // lam gi thi lam
       // noi dung cua trang chu
-      //   template: "<h1>Noi dung ve chung toi </h1>",
-      //   truyen 1 file html
-      templateUrl: "./pages/ve-chung-toi.html",
+      //   template: "<h1>Noi dung trang contact</h1>",
+      //Truyen file HTML
+      templateUrl: "pages/lien-he.html",
     })
-    .when("/lien-he", {
+    .when("/cua-hang", {
       // lam gi thi lam
       // noi dung cua trang chu
-      template: "<h1>Noi dung lien he</h1>",
+      template: "<h1>Noi dung trang cua hang</h1>",
     })
     .otherwise({
       redirectTo: "/trang-chu",

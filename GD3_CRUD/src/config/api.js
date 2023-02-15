e8839e5275054461d0fc680cfe4294e0b0224765
HangNT169 <HangNT169@fpt.edu.vn>
2023-02-15 12:32:55 +0700

    Finish CRUD

--- GD3_CRUD/src/config/api.js
+++ GD3_CRUD/src/config/api.js
@@ -2,4 +2,3 @@ const url = "http://localhost:3000";
 window.thucVatAPI = url + "/thucVat";
 window.dongVatAPI = url + "/dongVat";
 window.sinhVienAPI = url + "/sinhVien";
-// => http://localhost:3000 /sinhVien
--- GD3_CRUD/src/controller/home/thuc-vat-controller.js
+++ GD3_CRUD/src/controller/home/thuc-vat-controller.js
@@ -1,30 +1,26 @@
 window.ThucVatController = function ($scope, $http) {
-  // De giao tiep giua BE , FE
+  // De giao tiep BE, FE
   // => Phuong thuc HTTP
-  // Su dung HTTP Method: GET, POST, PUT, DELETE,...
-  // CRUD : CREATE, READ, UPDATE,DELETE
-  // HTTP STATUS CODE: 200,201,400,404,401,403,500...
-  // GET: Hien thi du lieu <=> READ <=> SELECT
+  // Voi cac HTTP Method : GET,POST,PUT,DELETE.....
+  // CRUD: CREATE, READ, UPDATE, DELETE
+  // request: Nhung gia tri tham so duoc truyen tu client => server
+  // (FE => BE) => Giong tham so truyen vao cua 1 function
+  // response: Gia tri nhan duoc tu phia server => client
+  // (BE=> FE)=> Giong nhu ket qua tra ve cua 1 function
+  // => Giong ket qua tra ve cua 1 function
   $scope.listThucVat = [];
-  $scope.request = {
+  $scope.form_thucVat = {
     ten: "",
     loai: "",
     gioiTinh: true,
   };
+  $scope.viTri = -1;
+  // GET: Lay du lieu  <=> READ <=> SELECT
   $http.get(thucVatAPI).then(function (response) {
     $scope.listThucVat = response.data;
   });
 
-  // Detail
-  $scope.detailThucVat = function (event, index) {
-    event.preventDefault();
-    let tv = $scope.listThucVat[index];
-    $scope.request.ten = tv.ten;
-    $scope.request.loai = tv.loai;
-    $scope.request.gioiTinh = tv.gioiTinh;
-  };
-
-  // Remove: Xoa du lieu <=> DELETE <=> DELETE
+  // DELETE <=> DELETE <=> DELETE
   $scope.removeThucVat = function (event, index) {
     event.preventDefault();
     let tv = $scope.listThucVat[index];
@@ -35,14 +31,33 @@ window.ThucVatController = function ($scope, $http) {
     });
   };
 
-  // Add <=> CREATE <=> POST
+  // Detail
+  $scope.detailThucVat = function (event, index) {
+    event.preventDefault();
+    let tv = $scope.listThucVat[index];
+    // console.log(tv);
+    // $scope.name = tv.name;
+    $scope.form_thucVat.ten = tv.ten;
+    $scope.form_thucVat.loai = tv.loai;
+    $scope.form_thucVat.gioiTinh = tv.gioiTinh;
+    $scope.viTri = index;
+  };
+
+  // POST: Them du lieu  <=> CREATE <=> INSERT INTO
   $scope.addThucVat = function (event) {
     event.preventDefault();
-    $http.post(thucVatAPI, $scope.request).then(function (response) {
-      $scope.listThucVat.push(response.data);
+    $http.post(thucVatAPI, $scope.form_thucVat).then(function (r) {
+      $scope.listThucVat.push(r.data);
     });
   };
 
-  // Update : PUT
-  $scope.updateThucVat = function (event) {};
+  // PUT: Thay doi du lieu <=> UPDATE <=> UPDATE
+  $scope.updateThucVat = function (event) {
+    event.preventDefault();
+    let tv = $scope.listThucVat[$scope.viTri];
+    let api = thucVatAPI + "/" + tv.id;
+    $http.put(api, $scope.form_thucVat).then(function (r) {
+      $scope.listThucVat[$scope.viTri] = r.data;
+    });
+  };
 };
